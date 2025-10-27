89de612f5ac918fc262deb7274b780da142070fd
Arthur Albuquerque <albuquerquearthur778@gmail.com>
2025-10-26 21:29:47 -0300

    Contexto do usuário agora recebe o id da conta

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,8 +1,8 @@
 import { createContext, ReactNode, useState } from "react";
 
 interface ContextType {
-    user: { nome: string, renda: number },
-    setUser: (user: { nome: string, renda: number }) => void
+    user: { id: number, nome: string, renda: number },
+    setUser: (user: { id: number, nome: string, renda: number }) => void
 }
 
 interface UserProviderProps {
@@ -12,7 +12,7 @@ interface UserProviderProps {
 export const UserContext = createContext<ContextType | undefined>(undefined);
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
-    const [ user, setUser ] =  useState({ nome: "", renda: 0 });
+    const [ user, setUser ] =  useState({ id: 0, nome: "", renda: 0 });
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
