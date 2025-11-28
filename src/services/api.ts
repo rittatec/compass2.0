d1179d12ef9a4ba995e759378e63227bfd1bcd8a
Arthur Albuquerque <lourencoarthur224@gmail.com>
2025-11-28 20:13:32 -0300

    Correção de IP interno exposto no Front-End e criação do arquivo .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import Config from 'react-native-config';
 
 // Aqui vou colocar a URL real da  API quando eu tiver essa url
-const API_URL = 'http://172.20.10.4:8080';
+const API_URL = Config.REACT_APP_API_URL || 'http://localhost:8080';
+
 export const api = axios.create({
   baseURL: API_URL,
-})
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
